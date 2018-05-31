b7708ed1e445f7c33c0cbd820cd2305c2eaabb60
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-05-31 12:34:28 +0800

    MovementIput basic

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementScript : MonoBehaviour {
+    Rigidbody2D playerRB;
+    public float maxVel;
+    Vector2 moveInput;
+    Vector2 moveInputRaw;
+    public float velMult;
+
+	void Start () {
+        playerRB = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+	}
+
+    private void FixedUpdate() {
+        moveInputRaw = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            playerRB.AddForce(moveInput * velMult);
+        if (playerRB.velocity.magnitude > maxVel) {
+            playerRB.AddForce(playerRB.velocity.normalized *- velMult);
+        }
+    }
+}
